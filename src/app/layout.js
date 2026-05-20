ceb125cc70330c0cc9eb7dca003631f88983fa6b
nijum-dev <jannatnijum.dev@gmail.com>
2026-05-20 21:05:47 +0600

    added an extra section of features

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Cards from "@/components/Cards";
 import Footer from "@/components/Footer";
+import ExtraOne from "@/components/Extraone";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <Navbar></Navbar>
         <Hero></Hero>
         <Cards></Cards>
+        <ExtraOne></ExtraOne>
         {children}
         <Footer></Footer>
         
---
+++ src/components/Extraone.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ExtraOne = () => {
+    return (
+        <div>
+            <div className='bg-[#1A3C2A] text-center items-center pt-15'>
+                <h5 className='text-[#E8A020] font-semibold text-sm '>SIMPLE PROCESS</h5>
+                <h1 className='text-white font-bold text-5xl mt-2'>How IdeaVault Works</h1>
+                <p className='text-gray-400 text-md mt-4'>Four steps from raw idea to community validation.</p>
+
+            </div>
+        </div>
+    );
+};
+<h5>SIMPLE PROCESS</h5>
+export default ExtraOne;
